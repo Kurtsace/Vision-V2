8128b679e4a115e8f207b5aef10c1fe666de78e3
Kurtsace <palokp@myumanitoba.ca>
2017-06-07 18:03:52 -0500

    Cmake

---
+++ cmake-build-debug/CMakeFiles/Vision_V2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Vision_V2.dir/main.cpp.o"
+  "CMakeFiles/Vision_V2.dir/Threshold.cpp.o"
+  "Vision_V2.pdb"
+  "Vision_V2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Vision_V2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
