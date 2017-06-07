777da3aa2d12555f577c35f7e440cc75334276f5
Kurtsace <palokp@myumanitoba.ca>
2017-06-07 17:03:03 -0500

    Update

--- main.cpp
+++ main.cpp
@@ -1,67 +1,46 @@
-#include "Threshold.h"
+#include <iostream>
+#include <opencv2/core/core_c.h>
+#include <opencv2/highgui/highgui.hpp>
+#include "Vision.h"
 
 using namespace std;
 using namespace cv;
 
-//Create 2 Mat objects, source and final
+
 Mat source, final;
 
-//Screen size
-int width = 640;
-int height = (width / 4) * 3;
+Vision v(&source, "RED", &final);
 
-//Create imshow windows
-void createWindows();
+int main(){
 
-int main(int argc, char** argv) {
+    v.detectCircle(true);
 
+    VideoCapture cap(1);
 
-
-    //Initialize object after creating source and final
-    Threshold red(&source, argv[1], &final);
-
-    VideoCapture cap(0);
-
-    //Check to see if opening the camera worked
     if(!cap.isOpened()){
 
-        cout << "Error opening camera" << endl;
+        cout << " ERROR OPENING CAMERA " << endl;
         return -1;
     }
 
-    //Infinite while loop
     while(1){
 
-        //Store the frame into source
-        bool success = cap.read(source);
+        if(!cap.read(source)){
 
-        //Check to see if reading frame was successful
-        if(!success){
-
-            cout << "Error reading frame from camera" << endl;
+            cout << "Error reading frame" << endl;
             break;
         }
 
-        source.copyTo(final);
-        final.setTo(Scalar(0, 0, 0));
+        v.start();
 
-        //Call start for the objects in the while loop
-        red.start();
+        namedWindow("Final", CV_WINDOW_AUTOSIZE);
+        imshow("Final", final);
 
-        red.setThreshold();
 
-        //Show output
-        createWindows();
-
-        //Break out of the loop if ESC key is pressed
         if(waitKey(30) == 27){
 
-            cout << "ESC key was pressed" << endl;
-
-            //Kill the initialized objects and destroy all existing windows
-            red.stop();
-            destroyAllWindows();
-
+            v.stop();
+            cout << "ESC was pressed" << endl;
             break;
         }
     }
@@ -69,15 +48,3 @@ int main(int argc, char** argv) {
     return 0;
 }
 
-//Create imshow windows
-void createWindows(){
-
-    //Create a single imshow window containing only final
-    namedWindow("Output", CV_WINDOW_AUTOSIZE);
-    imshow("Output", final);
-
-    //Create a small imshow window containing the source frame
-    //namedWindow("Source", CV_WINDOW_FREERATIO);
-    //resizeWindow("Source", 480, (480 / 4) * 3);
-    //imshow("Source", source);
-}
